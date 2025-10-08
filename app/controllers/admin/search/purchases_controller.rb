b417d87a8540d6a8b4c67ac932a0c2daca825119
Emmanuel Cousin <emmanuel@hey.com>
2025-10-08 17:55:28 -0300

    Update purchases_controller.rb

--- app/controllers/admin/search/purchases_controller.rb
+++ app/controllers/admin/search/purchases_controller.rb
@@ -8,7 +8,7 @@ class Admin::Search::PurchasesController < Admin::Search::BaseController
   def index
     super do |pagination, purchases|
       if purchases.one? && params[:page].blank?
-        redirect_to admin_purchase_path(purchases.first) && return
+        return redirect_to admin_purchase_path(purchases.first)
       end
     end
   end
@@ -21,7 +21,7 @@ class Admin::Search::PurchasesController < Admin::Search::BaseController
     def search_params
       {
         query: params[:query].to_s.strip,
-        product_title_query: params[:product_title_query].to_s.strip,
+        product_title_query: params[:product_title_query].to_s.strip.presence,
         purchase_status: params[:purchase_status]
       }
     end
