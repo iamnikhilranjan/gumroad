8d6fa312898b6f4ec01f43e8ad238583525df1b8
Emmanuel Cousin <emmanuel@hey.com>
2025-10-16 21:01:32 -0300

    Update index.tsx

--- app/javascript/pages/Customers/index.tsx
+++ app/javascript/pages/Customers/index.tsx
@@ -1,7 +1,7 @@
 import { usePage } from "@inertiajs/react";
 import React from "react";
 
-import { default as CustomersPage, CustomerPageProps } from "$app/components/Audience/Customers";
+import CustomersPage, { type CustomerPageProps } from "$app/components/Audience/Customers";
 
 function index() {
   const { customers_presenter } = usePage<{ customers_presenter: CustomerPageProps }>().props;
