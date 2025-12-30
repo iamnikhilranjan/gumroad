79027d10634daa2e50968d16146c6c381a2f9e17
Emmanuel Cousin <emmanuel@hey.com>
2025-12-30 17:13:08 +0100

    Revert "remove duplicate prop"
    This reverts commit 2e5dc5e2902bc4611eb51f55feab42ad91a45bd9.

--- app/presenters/auth_presenter.rb
+++ app/presenters/auth_presenter.rb
@@ -20,6 +20,7 @@ class AuthPresenter
     referrer = User.find_by_username(params[:referrer]) if params[:referrer].present?
     number_of_creators, total_made = $redis.mget(RedisKey.number_of_creators, RedisKey.total_made)
     login_props.merge(
+      recaptcha_site_key: GlobalConfig.get("RECAPTCHA_SIGNUP_SITE_KEY"),
       referrer: referrer ? {
         id: referrer.external_id,
         name: referrer.name_or_username,
